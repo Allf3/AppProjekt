2194e9dfc0809634c961d619158e978c261a5295
Martin Nørby Høj Jørgensen <mnj@eucsyd.dk>
2021-04-23 13:15:35 +0200

    Behaviors

---
+++ AppUI/AppUI/Behaviors/TextValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace AppUI.Behaviors
+{
+    public class TextValidator : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor editor)
+        {
+            editor.TextChanged += OnEditorTextChanged;
+            base.OnAttachedTo(editor);
+        }
+
+        protected override void OnDetachingFrom(Editor editor)
+        {
+            editor.TextChanged -= OnEditorTextChanged;
+            base.OnDetachingFrom(editor);
+        }
+
+        private void OnEditorTextChanged(object sender, TextChangedEventArgs args)
+        {
+            Regex reg = new Regex("[0-9]");
+            bool isValid = reg.IsMatch(args.NewTextValue);
+            ((Editor)sender).TextColor = isValid ? Color.Red : Color.Default;
+            ((Editor)sender).BackgroundColor = isValid ? Color.FromHex("#FBC5D0") : Color.Default;
+        }
+    }
+}
